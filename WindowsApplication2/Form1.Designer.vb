0eaef7a1b20efc0cac1f82f014ffef4477eb3cf1
zxc6310 <bleach20115@yahoo.com.tw>
2015-10-28 23:30:59 +0800

    https://github.com/zxc6310/20151029.git

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btenter = New System.Windows.Forms.Button()
+        Me.btesc = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(58, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "帳號"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("新細明體", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(58, 266)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "密碼"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Font = New System.Drawing.Font("新細明體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txtUserName.Location = New System.Drawing.Point(312, 62)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(342, 36)
+        Me.txtUserName.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("新細明體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(312, 266)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(342, 36)
+        Me.txtPassword.TabIndex = 3
+        '
+        'btenter
+        '
+        Me.btenter.Location = New System.Drawing.Point(62, 418)
+        Me.btenter.Name = "btenter"
+        Me.btenter.Size = New System.Drawing.Size(138, 46)
+        Me.btenter.TabIndex = 4
+        Me.btenter.Text = "登入"
+        Me.btenter.UseVisualStyleBackColor = True
+        '
+        'btesc
+        '
+        Me.btesc.Location = New System.Drawing.Point(516, 418)
+        Me.btesc.Name = "btesc"
+        Me.btesc.Size = New System.Drawing.Size(138, 46)
+        Me.btesc.TabIndex = 5
+        Me.btesc.Text = "離開"
+        Me.btesc.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(807, 575)
+        Me.Controls.Add(Me.btesc)
+        Me.Controls.Add(Me.btenter)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btenter As System.Windows.Forms.Button
+    Friend WithEvents btesc As System.Windows.Forms.Button
+
+End Class
